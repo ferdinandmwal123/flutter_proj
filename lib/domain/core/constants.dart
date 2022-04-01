61b883247e0e92c83f01dbb47a53b9b31044f50c
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-01 14:49:46 +0300

    completed API class

--- lib/domain/core/constants.dart
+++
@@ -1,3 +0,0 @@
-// ignore_for_file: constant_identifier_names
-
-const String BASE_URL = 'https://api.covid19api.com/summary';
---
+++ lib/infrastructure/remote/covid_api_service.chopper.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'covid_api_service.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
+class _$CovidApiService extends CovidApiService {
+  _$CovidApiService([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final definitionType = CovidApiService;
+
+  @override
+  Future<Response<dynamic>> getGlobal() {
+    final $url = '/summary';
+    final $request = Request('GET', $url, client.baseUrl);
+    return client.send<dynamic, dynamic>($request);
+  }
+}
