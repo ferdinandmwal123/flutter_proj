33c2e4466eed3f3a764661fe310d2ee053121c06
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-01 13:31:13 +0300

    configured dependency injection with injectable package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/injection.dart';
+import 'package:injectable/injectable.dart';
 
 void main() {
+  configureDependencies(Environment.prod);
   runApp(const MyApp());
 }
 
