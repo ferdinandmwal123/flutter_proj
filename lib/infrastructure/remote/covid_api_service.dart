e0efd6de0dd6fe1a660462e3e77036810402134d
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-03 15:24:59 +0300

    added logger dependency and removed @factoryMethod

--- lib/infrastructure/remote/covid_api_service.dart
+++ lib/infrastructure/remote/covid_api_service.dart
@@ -2,17 +2,18 @@ import 'package:chopper/chopper.dart';
 import 'package:flutter_project/infrastructure/core/constants.dart';
 import 'package:flutter_project/infrastructure/dto/covid_data_dtos/global_dto.dart';
 import 'package:injectable/injectable.dart';
+import 'package:logger/logger.dart';
 
 part 'covid_api_service.chopper.dart';
 
 @LazySingleton()
 @ChopperApi(baseUrl: '/summary')
 abstract class CovidApiService extends ChopperService {
+  var logger = Logger();
   @Get()
   Future<Response<GlobalDto>> getGlobal();
-  //! Response is of type Global or Global Dto?
 
-  @factoryMethod //injectable will automatically register it as an asynchronous factory because the return type is a Future.
+  // @factoryMethod  //injectable will automatically register it as an asynchronous factory because the return type is a Future.
   static CovidApiService create() {
     final client = ChopperClient(
         baseUrl: BASE_URL,
@@ -22,4 +23,6 @@ abstract class CovidApiService extends ChopperService {
         converter: const JsonConverter());
     return _$CovidApiService(client);
   }
+  //? TEST: Test the creation of this service asynchronously
+
 }
