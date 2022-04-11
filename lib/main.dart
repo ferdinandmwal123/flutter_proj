a269043e4c0b277ba5ffc6cbf2ef50de0e822650
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-11 14:07:34 +0300

    removed starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/injection.dart';
+import 'package:flutter_project/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
