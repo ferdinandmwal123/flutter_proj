5075f9e6966bfea29456c93f42db62e6bc4c5133
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-12 14:05:21 +0300

    splash page

---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project/application/bloc/covid_watcher_bloc.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<CovidWatcherBloc, CovidWatcherState>(
+      listener: (context, state) {
+        state.map(
+            initial: (_) => {},
+            loadInProgress: loadInProgress,
+            loadComplete: loadComplete,
+            loadSuccess: loadSuccess,
+            loadFailure: (_) => {});
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
