886ebd334504b348c8b9c69cfd29f75a08aa0cd1
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-12 13:39:46 +0300

    app widget bloc config

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project/application/bloc/covid_watcher_bloc.dart';
+import 'package:flutter_project/injection.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<CovidWatcherBloc>()..add(const CovidWatcherEvent.watchDataStarted()),
+        )
+        
+      ], child: MaterialApp()); 
   }
 }
