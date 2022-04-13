d3e040662cdf39ff9b52199b956230dc6ba94807
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-13 13:50:08 +0300

    completed presentation layer setup

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -11,6 +11,7 @@ class AppWidget extends StatelessWidget {
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
       title: 'Covid',
+      debugShowCheckedModeBanner: false,
       
     );
   }
