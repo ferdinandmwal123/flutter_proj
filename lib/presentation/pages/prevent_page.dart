9293fa0c16bc33fa2a90af4f29a99886d927a6b6
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-13 13:35:42 +0300

    prevent page

---
+++ lib/presentation/pages/prevent_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PreventPage extends StatelessWidget {
+  const PreventPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
