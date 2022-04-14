54a0081d589e14b4d2591d53815da9246ffff382
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-14 13:10:36 +0300

    fl_chart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/injection.dart';
 import 'package:flutter_project/presentation/core/app_widget.dart';
 import 'package:injectable/injectable.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
   //*INFO:  the plugin needs to use platform channels to call the native code, which is done asynchronously therefore you have to call ensureInitialized() to make sure that you have an instance of the WidgetsBinding.
@@ -9,10 +10,11 @@ void main() async {
   configureDependencies();
   //* INFO: flutter_native_splash
   await initialization(null);
-  runApp( AppWidget());
+  runApp(AppWidget());
 }
 
 Future initialization(BuildContext? context) async {
   // you can do stuff here
   await Future.delayed(const Duration(seconds: 3));
+  // FlutterNativeSplash.remove();
 }
