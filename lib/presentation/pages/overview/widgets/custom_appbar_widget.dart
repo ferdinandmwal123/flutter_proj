a757ff58337da9fda87732f00b3a72444fba4b7c
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-22 13:31:34 +0300

    completed custom appbar

--- lib/presentation/pages/overview/widgets/custom_appbar_widget.dart
+++ lib/presentation/pages/overview/widgets/custom_appbar_widget.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({ Key? key }) : super(key: key);
-
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return AppBar(
+      backgroundColor: Colors.blueAccent,
+      elevation: 0.0,
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        iconSize: 28.0,
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.notifications_none),
+          iconSize: 28.0,
+          onPressed: () {},
+        ),
+      ],
     );
   }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
--- lib/presentation/pages/overview_page.dart
+++ lib/presentation/pages/overview_page.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'overview/widgets/custom_appbar_widget.dart';
 
-class OverviewPage extends StatelessWidget {
-  const OverviewPage({Key? key}) : super(key: key);
+class OverviewPage extends StatefulWidget {
+  const OverviewPage({ Key? key }) : super(key: key);
 
   @override
+  State<OverviewPage> createState() => _OverviewPageState();
+}
+
+class _OverviewPageState extends State<OverviewPage> {
+  @override
   Widget build(BuildContext context) {
-    return CustomAppBar(),
+    return Scaffold(
+      appBar: CustomAppBar(),
+     
+    );
   }
 }
