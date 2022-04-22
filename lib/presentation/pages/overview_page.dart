150f52010ebaf639c97e9010fc3762abf5cab765
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-22 13:22:35 +0300

    custom appbar

--- lib/presentation/pages/overview_page.dart
+++ lib/presentation/pages/overview_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'overview/widgets/custom_appbar_widget.dart';
+
 class OverviewPage extends StatelessWidget {
   const OverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Overview '),);
+    return CustomAppBar(),
   }
 }
