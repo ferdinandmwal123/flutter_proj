d451134b91b92ac9e28e5ded6e8ac2dbe4a5369e
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-30 14:32:36 +0300

    clipper

--- lib/presentation/pages/about/about_page.dart
+++ lib/presentation/pages/about/about_page.dart
@@ -7,18 +7,42 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          width: double.infinity,
-          decoration:const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                Color(0xFF473F97),
-                Color.fromARGB(255, 114, 104, 230)
-              ])),
+        ClipPath(
+          clipper: MyClipper(),
+          child: Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFF473F97),
+                      Color.fromARGB(255, 114, 104, 230)
+                    ]),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/virus.png"),
+                )),
+          ),
         )
       ],
     );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
