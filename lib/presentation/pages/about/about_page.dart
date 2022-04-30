cd42c06ae911f93c6975cddc495802e81bbad6d0
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-04-30 14:26:36 +0300

    virus png

--- lib/presentation/pages/about/about_page.dart
+++ lib/presentation/pages/about/about_page.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({ Key? key }) : super(key: key);
+  const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('About')),
-      
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration:const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                Color(0xFF473F97),
+                Color.fromARGB(255, 114, 104, 230)
+              ])),
+        )
+      ],
     );
   }
 }
