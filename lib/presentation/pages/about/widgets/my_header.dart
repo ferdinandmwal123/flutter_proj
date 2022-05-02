ae377e90a9af1ee5b49fbe23bb4bacc4dd948c09
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-05-02 17:43:09 +0300

    refactor

--- lib/presentation/pages/about/widgets/my_header.dart
+++ lib/presentation/pages/about/widgets/my_header.dart
@@ -55,15 +55,16 @@ class MyHeader extends StatelessWidget {
                                 fontSize: 22, fontWeight: FontWeight.w200)
                             .copyWith(color: Colors.white)),
                   ),
-                  // Positioned(
-                  //   child: Text("COVID-19 \nChina \n3/12/2019",
-                  //       style: const TextStyle(
-                  //               fontSize: 22, fontWeight: FontWeight.w900)
-                  //           .copyWith(color: Colors.white)),
-                  //   top: 20,
-                  //   left: 240,
-                  // ),
-                  Text("data")
+                  Positioned(
+                    child: Text("COVID-19 \nChina \n30/2/2019",
+                        style: const TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w900)
+                            .copyWith(color: Colors.white)),
+                    top: 20,
+                    left: 240,
+                  ),
+                  //! UI WARNING: Do not remove this empty container or die!!!!!!
+                  Container()
                 ],
               ),
             )
