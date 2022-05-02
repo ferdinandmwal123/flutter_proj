5d91fb449aa54710a1d796de5518e7b7df478f66
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-05-02 17:10:09 +0300

    about header

--- lib/presentation/pages/about/about_page.dart
+++ lib/presentation/pages/about/about_page.dart
@@ -1,48 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/my_header.dart';
+
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        ClipPath(
-          clipper: MyClipper(),
-          child: Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xFF473F97),
-                      Color.fromARGB(255, 114, 104, 230)
-                    ]),
-                image: DecorationImage(
-                  image: AssetImage("assets/images/virus.png"),
-                )),
-          ),
+      children: const [
+         MyHeader(
+          image: "assets/icons/coronadr.svg",
+          textTop: "Avoid crowds",
+          textBottom: "stay at home.",
         )
       ],
     );
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
---
+++ lib/presentation/pages/about/widgets/my_header.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyHeader extends StatelessWidget {
+  final String? image;
+  final String? textTop;
+  final String? textBottom;
+  const MyHeader({
+    Key? key,
+    this.image,
+    this.textTop,
+    this.textBottom,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: const EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 310.0,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF473F97),
+              Color(0xFF473F97) ,
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage("assets/images/virus.png"),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: Stack(
+                children: [
+                  SvgPicture.asset(
+                    image!,
+                    width: 230,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+                  Positioned(
+                    top: 20,
+                    left: 150,
+                    child: Text("$textTop \n$textBottom",
+                        style: const TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.w600)
+                            .copyWith(color: Colors.white)),
+                  ),
+                  Container()
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
