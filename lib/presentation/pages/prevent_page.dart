d8634ae5ef806dd1d6dda80f6f05a0d7051aa4ff
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-05-12 14:51:42 -0400

    own test svg

--- lib/presentation/pages/prevent_page.dart
+++ lib/presentation/pages/prevent_page.dart
@@ -7,6 +7,7 @@ class PreventPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return SingleChildScrollView(
       child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         const PreventHeader(
@@ -46,6 +47,28 @@ class PreventPage extends StatelessWidget {
                     title: "Disinfect",
                   )
                 ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.orange,
+                height: screenHeight * 0.15,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Color(0xFFAD9FE4), Color(0xFF473F97)]),
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset('assets/images/own_test.png')
+                  ],
+                ),
               )
             ],
           ),
@@ -70,7 +93,10 @@ class PreventionCard extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SvgPicture.asset(svgSrc),
-        Text(title, style: Theme.of(context).textTheme.bodyText2
+        Text(title,
+            style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                fontWeight: FontWeight.w500,
+                color: const Color.fromARGB(255, 91, 95, 94))
             // .copyWith(color: const Color(0xFF0D8E53)),
             )
       ],
