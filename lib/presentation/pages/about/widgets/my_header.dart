c31e2ec84d40c464bbaa3c251e1bac5040cb6d46
ferdinandmwal123 <ferdinandmwal@gmail.com>
2022-05-12 10:26:31 -0400

    prevent header

--- lib/presentation/pages/about/widgets/my_header.dart
+++ lib/presentation/pages/about/widgets/my_header.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+//TODO(04): Refactor two headers to become one
+
 class AboutHeader extends StatelessWidget {
   final String? image;
   final String? textTop;
@@ -65,7 +67,64 @@ class AboutHeader extends StatelessWidget {
                   ),
                   //! UI WARNING: Do not remove this empty container or die!!!!!!
                   Container(),
-                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventHeader extends StatelessWidget {
+  final String? image;
+  final String? textTop;
+  final String? textBottom;
+  const PreventHeader({
+    Key? key,
+    this.image,
+    this.textTop,
+    this.textBottom,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: const EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 310.0,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF473F97),
+              Color(0xFF473F97),
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage("assets/images/virus.png"),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: Stack(
+                children: [
+                  SvgPicture.asset(
+                    image!,
+                    width: 230,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
+
+                  //! UI WARNING: Do not remove this empty container or die!!!!!!
+                  Container(),
                 ],
               ),
             )
